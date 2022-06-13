476d2d820aea4d4432ea8e02430a6d4cf7d64c63
Rafal Lagowski <rafal@lagowski.es>
2022-06-13 16:26:38 +0200

    celan file

--- app/api/api_v1/endpoints/predicthouseprice.py
+++ app/api/api_v1/endpoints/predicthouseprice.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter, Request
-from typing import Optional
 from app.models.MLR_PredictingHousePrices.predict import PredictHousePrice
-from pydantic import BaseModel
 
 ph = PredictHousePrice()
 router = APIRouter()
@@ -9,8 +7,8 @@ router = APIRouter()
 @router.post("/predicthouseprice")
 async def getInformation(info : Request):
     req_info = await info.json()
-    res = ph.getPrice([req_info["data"]])
+    res = ph.getPrice(req_info["data"])
     return {
         "status" : "SUCCESS",
-        "prediction" : res[0]
+        "prediction" : res
     }
